24d6c4449451c095a7ff8a1e30bda644792c5fb2
Muhammad Shaheer Abbas <shaheerabbas20@gmail.com>
2020-10-04 20:57:17 +0500

    ui with all encrypt

---
+++ CipherAll/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace CipherAll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Key = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(381, 174);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 40);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(381, 81);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(407, 60);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(381, 247);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(407, 60);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Key
+            // 
+            this.Key.AutoSize = true;
+            this.Key.Location = new System.Drawing.Point(341, 186);
+            this.Key.Name = "Key";
+            this.Key.Size = new System.Drawing.Size(25, 13);
+            this.Key.TabIndex = 4;
+            this.Key.Text = "Key";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Message";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(336, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ceaser",
+            "Vegenere",
+            "RailFence",
+            "PlayFair"});
+            this.comboBox1.Location = new System.Drawing.Point(381, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(252, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(333, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ciphers";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 451);
+            this.panel1.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(86, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 140);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(248, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Welcome To Cipher World";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Key);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Key;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CipherAll/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CipherAll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {  
+            //int x = Convert.ToInt32(textBox1.Text);
+            string message = textBox2.Text;
+            
+            if(comboBox1.Text == "Vegenere")
+            {
+                string x = textBox1.Text;
+                textBox3.Text = VegenereCipher.encrypt(message, x);
+            }
+
+            if (comboBox1.Text == "Ceaser")
+            {
+                int x = Convert.ToInt32(textBox1.Text);
+                textBox3.Text = ceasercipher.encrypt(message, x);
+            }
+
+            if (comboBox1.Text == "RailFence")
+            {
+                int x = Convert.ToInt32(textBox1.Text);
+                textBox3.Text = RailfenceCipher.encrypt(message, x);
+            }
+
+            if (comboBox1.Text == "PlayFair")
+            {
+                string x = textBox1.Text;
+                textBox3.Text = Playfair.encrypt(message, x, false);
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+public class ceasercipher
+{
+    public static string encrypt(string text, int key)
+    {
+        string result = " ";
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            if (char.IsUpper(text[i]))
+            {
+                result += Convert.ToChar(Convert.ToInt32(text[i] + key - 65) % 26 + 65);
+            }
+            else
+            {
+
+                result += Convert.ToChar(Convert.ToInt32(text[i] + key - 97) % 26 + 97);
+            }
+        }
+        return result;
+    }
+}
+
+public class VegenereCipher
+{
+    public static string encrypt(string text, string key)
+    {
+        string result = "";
+
+         for (int i = 0; i < text.Length; i++)
+         {
+          result += Convert.ToChar(((Convert.ToInt32(text[i]) - 97)+(Convert.ToInt32(key[i])- 97))%26 +97) ;
+         }
+
+        return result;
+    }
+}
+
+
+
+public class RailfenceCipher
+{
+    public static string encrypt(string text, int key)
+    {
+        string result = string.Empty;
+
+        char[][] matrix = BuildCleanMatrix(key, text.Length);
+
+        int rowIncrement = 1;
+        for (int row = 0, col = 0; col < matrix[row].Length; col++)
+        {
+            if (
+                row + rowIncrement == matrix.Length ||
+                row + rowIncrement == -1
+                )
+            {
+                rowIncrement *= -1;
+            }
+
+            matrix[row][col] = text[col];
+
+            row += rowIncrement;
+        }
+
+        result = BuildStringFromMatrix(matrix);
+
+        return result;
+    }  
+        public static char[][] BuildCleanMatrix(int rows, int cols)
+        {
+            char[][] result = new char[rows][];
+
+            for (int row = 0; row < result.Length; row++)
+            {
+                result[row] = new char[cols];
+            }
+
+            return result;
+        }
+
+        public static string BuildStringFromMatrix(char[][] matrix)
+        {
+            string result = string.Empty;
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (matrix[row][col] != '\0')
+                    {
+                        result += matrix[row][col];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static char[][] Transpose(char[][] matrix)
+        {
+            char[][] result =
+                BuildCleanMatrix(matrix[0].Length, matrix.Length);
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    result[col][row] = matrix[row][col];
+                }
+            }
+
+            return result;
+        }
+
+    
+}
+
+
+
+public class Playfair
+{
+    public static string encrypt(string input, string key, bool encipher)
+    {
+        string retVal = string.Empty;
+        char[,] keySquare = GenerateKeySquare(key);
+        string tempInput = RemoveOtherChars(input);
+        int e = encipher ? 1 : -1;
+
+        if ((tempInput.Length % 2) != 0)
+            tempInput += "X";
+
+        for (int i = 0; i < tempInput.Length; i += 2)
+        {
+            int row1 = 0;
+            int col1 = 0;
+            int row2 = 0;
+            int col2 = 0;
+
+            GetPosition(ref keySquare, char.ToUpper(tempInput[i]), ref row1, ref col1);
+            GetPosition(ref keySquare, char.ToUpper(tempInput[i + 1]), ref row2, ref col2);
+
+            if (row1 == row2 && col1 == col2)
+            {
+                retVal += new string(SameRowColumn(ref keySquare, row1, col1, e));
+            }
+            else if (row1 == row2)
+            {
+                retVal += new string(SameRow(ref keySquare, row1, col1, col2, e));
+            }
+            else if (col1 == col2)
+            {
+                retVal += new string(SameColumn(ref keySquare, col1, row1, row2, e));
+            }
+            else
+            {
+                retVal += new string(DifferentRowColumn(ref keySquare, row1, col1, row2, col2));
+            }
+        }
+
+        retVal = AdjustOutput(input, retVal);
+
+        return retVal;
+    }
+
+    public static int Mod(int a, int b)
+    {
+        return (a % b + b) % b;
+    }
+
+    public static List<int> FindAllOccurrences(string str, char value)
+    {
+        List<int> indexes = new List<int>();
+
+        int index = 0;
+        while ((index = str.IndexOf(value, index)) != -1)
+            indexes.Add(index++);
+
+        return indexes;
+    }
+
+    public static string RemoveAllDuplicates(string str, List<int> indexes)
+    {
+        string retVal = str;
+
+        for (int i = indexes.Count - 1; i >= 1; i--)
+            retVal = retVal.Remove(indexes[i], 1);
+
+        return retVal;
+    }
+
+    public static char[,] GenerateKeySquare(string key)
+    {
+        char[,] keySquare = new char[5, 5];
+        string defaultKeySquare = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+        string tempKey = string.IsNullOrEmpty(key) ? "CIPHER" : key.ToUpper();
+
+        tempKey = tempKey.Replace("J", "");
+        tempKey += defaultKeySquare;
+
+        for (int i = 0; i < 25; ++i)
+        {
+            List<int> indexes = FindAllOccurrences(tempKey, defaultKeySquare[i]);
+            tempKey = RemoveAllDuplicates(tempKey, indexes);
+        }
+
+        tempKey = tempKey.Substring(0, 25);
+
+        for (int i = 0; i < 25; ++i)
+            keySquare[(i / 5), (i % 5)] = tempKey[i];
+
+        return keySquare;
+    }
+
+    public static void GetPosition(ref char[,] keySquare, char ch, ref int row, ref int col)
+    {
+        if (ch == 'J')
+            GetPosition(ref keySquare, 'I', ref row, ref col);
+
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j)
+                if (keySquare[i, j] == ch)
+                {
+                    row = i;
+                    col = j;
+                }
+    }
+
+    public static char[] SameRow(ref char[,] keySquare, int row, int col1, int col2, int encipher)
+    {
+        return new char[] { keySquare[row, Mod((col1 + encipher), 5)], keySquare[row, Mod((col2 + encipher), 5)] };
+    }
+
+    public static char[] SameColumn(ref char[,] keySquare, int col, int row1, int row2, int encipher)
+    {
+        return new char[] { keySquare[Mod((row1 + encipher), 5), col], keySquare[Mod((row2 + encipher), 5), col] };
+    }
+
+    public static char[] SameRowColumn(ref char[,] keySquare, int row, int col, int encipher)
+    {
+        return new char[] { keySquare[Mod((row + encipher), 5), Mod((col + encipher), 5)], keySquare[Mod((row + encipher), 5), Mod((col + encipher), 5)] };
+    }
+
+    public static char[] DifferentRowColumn(ref char[,] keySquare, int row1, int col1, int row2, int col2)
+    {
+        return new char[] { keySquare[row1, col2], keySquare[row2, col1] };
+    }
+
+    public static string RemoveOtherChars(string input)
+    {
+        string output = input;
+
+        for (int i = 0; i < output.Length; ++i)
+            if (!char.IsLetter(output[i]))
+                output = output.Remove(i, 1);
+
+        return output;
+    }
+
+    public static string AdjustOutput(string input, string output)
+    {
+        StringBuilder retVal = new StringBuilder(output);
+
+        for (int i = 0; i < input.Length; ++i)
+        {
+            if (!char.IsLetter(input[i]))
+                retVal = retVal.Insert(i, input[i].ToString());
+
+            if (char.IsLower(input[i]))
+                retVal[i] = char.ToLower(retVal[i]);
+        }
+
+        return retVal.ToString();
+    }
+}
